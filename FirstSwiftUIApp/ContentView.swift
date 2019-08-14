1c6f6e3970c351f74ffe12be92c6b66fb7d01869
Atil Samancioglu <samancioglu.atil@gmail.com>
2019-08-14 10:53:59 +0300

    Initial Commit

---
+++ FirstSwiftUIApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  FirstSwiftUIApp
+//
+//  Created by Atil Samancioglu on 14.08.2019.
+//  Copyright © 2019 Atil Samancioglu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
